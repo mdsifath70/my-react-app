46edbc9220ae07f3d93cc191c7f24085378c5886
Sifath <sifathislam0@gmail.com>
2021-12-20 18:18:22 +0600

    react app

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 
 // Firebase Configuration
 const app = initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
+    // apiKey: process.env.REACT_APP_API_KEY,
+    apiKey: 'AIzaSyD--uvyPvrx4WEgBVpI2YJ7lw2prmMEVSs',
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
 });
 
 export default app;
